6d37347cf8f945f94fcb459cff11529cf3f63734
Mahdi Malverdi <mahdi.malverdi@gmail.com>
2018-12-13 20:27:17 +0330

    create graph

--- gen/main.py
+++ gen/main.py
@@ -5,35 +5,33 @@ from LuLu2GrammarListener import LuLu2GrammarListener
 from LuLu2GrammarParser import LuLu2GrammarParser
 from antlr4 import tree
 
-import plotly.plotly as py
-import plotly.graph_objs as go
+from pptree import *
 
-import igraph
-from igraph import *
-igraph.__version__
-
+root = Node("ROOT")
 
 def handleExpression(expr, level=0):
-    adding = True
-    value = 0
+    global root
     for child in expr.getChildren():
         if isinstance(child, tree.Tree.TerminalNode):
-            print("| "*(level-1)+"|_",child)
+            Node(child, root)
         else:
-            print("| "*(level-1)+"|_","start", LuLu2GrammarParser.ruleNames[expr.getRuleIndex()])
+            newNode = Node(LuLu2GrammarParser.ruleNames[child.getRuleIndex()], root)
+            tmp = root
+            root = newNode
             handleExpression(child, level+1)
-            print("| "*(level-1)+"|_","end", LuLu2GrammarParser.ruleNames[expr.getRuleIndex()])
-
+            root = tmp
 
 
 def main():
+    global root
     input = FileStream("test10.lulu")
     lexer = LuLu2GrammarLexer(input)
     stream = CommonTokenStream(lexer)
     parser = LuLu2GrammarParser(stream)
     tree = parser.program()
+    root = Node(LuLu2GrammarParser.ruleNames[tree.getRuleIndex()])
     handleExpression(tree)
-
+    print_tree(root)
 
 if __name__ == '__main__':
     main()
